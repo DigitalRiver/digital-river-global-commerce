c5fae4ff06a6d630e3ca1c5a1f4d17149e30457e
Edison Wu <ewu@digitalriver.com>
2020-08-21 13:14:42 +0800

    Issue-410 country and currency dropdowns are broken

--- public/partials/drgc-locale-selector.php
+++ public/partials/drgc-locale-selector.php
@@ -19,17 +19,17 @@
 ?>
 
 <?php if ( ! empty( $drgc_locale_options ) ) { ?>
-<li class="dropdown menu-item <?php echo $has_other_locales ? 'menu-item-has-children' : '' ?>" id="dr-locale-selector">
-  <a class="<?php echo $has_other_locales ? 'dropdown-toggle' : '' ?> nav-link dr-current-locale" data-dr-locale="<?php echo $current_locale ?>" href="#">
+<li class="dr-dropdown menu-item <?php echo $has_other_locales ? 'menu-item-has-children' : '' ?>" id="dr-locale-selector">
+  <a class="<?php echo $has_other_locales ? 'dr-dropdown-toggle' : '' ?> nav-link dr-current-locale" data-dr-locale="<?php echo $current_locale ?>" href="#">
     <span class="dr-flag-icon <?php echo get_dr_country_code( $current_locale ) ?>"></span>
     <?php echo __( get_dr_country_name( $current_locale ), 'digital-river-global-commerce' ) ?>
   </a>
   <?php if ( $has_other_locales ) { ?>
-  <ul class="dropdown-menu dr-other-locales">
+  <ul class="dr-dropdown-menu dr-other-locales">
   <?php foreach ( $drgc_locale_options as $locale_option ) { ?>
     <?php if ( $current_locale !== $locale_option['dr_locale'] ) { ?>
       <li class="menu-item nav-item">
-        <a class="dropdown-item" data-dr-locale="<?php echo $locale_option['dr_locale'] ?>" href="#">
+        <a class="dr-dropdown-item" data-dr-locale="<?php echo $locale_option['dr_locale'] ?>" href="#">
           <span class="dr-flag-icon <?php echo get_dr_country_code( $locale_option['dr_locale'] ) ?>"></span>
           <?php echo __( get_dr_country_name( $locale_option['dr_locale'] ), 'digital-river-global-commerce' ) ?>
         </a>
