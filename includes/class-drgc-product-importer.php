5951e604d5030959c8ec40b4fc21a4d4f96f1d9f
Cheryl Yu <cyu@digitalriver.com>
2020-10-14 19:24:50 +0800

    Fixed MySQL overload error by only caching all product ids

--- includes/class-drgc-product-importer.php
+++ includes/class-drgc-product-importer.php
@@ -67,15 +67,16 @@ class DRGC_Product_Importer {
   }
 
   public function fetch_and_cache_products() {
-    $res = DRGC()->site->get_products( $this->basic_product_query_params() );
+    $res = DRGC()->site->get_products( array( 'expand' => 'product.id' ) );
     $this->dr_products = $res['products']['product'] ?? array();
 
-    if ( count( $this->dr_products ) > 0 ) {
+    if ( ! empty( $this->dr_products ) ) {
       $this->recursive_fetch_more_products( $res['products'] );
       $this->filter_out_share_product();
+      $dr_product_ids = array_column( $this->dr_products, 'id' );
 
-      // Cache dr_products at most 1 hour
-      set_transient( 'dr_products', $this->dr_products, HOUR_IN_SECONDS );
+      // Cache dr_product_ids at most 1 hour
+      set_transient( 'dr_product_ids', $dr_product_ids, HOUR_IN_SECONDS );
       if ( $this->type === 'AJAX' ) {
         wp_send_json_success( $this->dr_products );
       } else {
@@ -92,7 +93,7 @@ class DRGC_Product_Importer {
 
   private function recursive_fetch_more_products( $products_data ) {
     if ( isset( $products_data['nextPage'] ) ) {
-      $res = DRGC()->site->fetch_data_by_uri( $products_data['nextPage']['uri'], $this->basic_product_query_params() );
+      $res = DRGC()->site->fetch_data_by_uri( $products_data['nextPage']['uri'], array( 'expand' => 'product.id' ) );
       $next_page_products = $res['products']['product'] ?? array();
       $this->dr_products = array_merge( $this->dr_products, $next_page_products );
       $this->recursive_fetch_more_products( $res['products'] );
@@ -100,12 +101,16 @@ class DRGC_Product_Importer {
   }
 
   public function import_each_product( $idx = null ) {
-    $this->dr_products = get_transient( 'dr_products' );
+    $dr_product_ids = get_transient( 'dr_product_ids' );
     $idx = ( $this->type === 'AJAX' && isset( $_POST['idx'] ) ) ? intval( $_POST['idx'] ) : $idx;
 
-    if ( false !== $this->dr_products && isset( $idx ) ) {
-      $product = $this->dr_products[$idx];
-      $gc_id = $product['id'] ?? 0;
+    if ( false !== $dr_product_ids && isset( $idx ) ) {
+      $gc_id = $dr_product_ids[$idx];
+      $product = DRGC()->product_details->get_product_details( $gc_id, array( 'expand' => 'all' ) );
+
+      if ( empty( $product ) ) {
+        wp_send_json_error( array( 'error' => __( 'Something went wrong when getting product ' . $gc_id . '.', 'digital-river-global-commerce' ) ) );
+      }
 
       // Get terms ID of product categories
       $categories_res = DRGC()->site->get_product_categories( $gc_id );
@@ -128,8 +133,8 @@ class DRGC_Product_Importer {
       // Variation product
       if ( isset( $product['variations'] ) ) {
         foreach ( $product['variations']['product'] as $key => $variation_product ) {
-          $gc_id = $variation_product['id'] ?? 0;
-          $variation_post_id  = drgc_get_product_by_gc_id( $gc_id, true );
+          $_gc_id = $variation_product['id'] ?? 0;
+          $variation_post_id  = drgc_get_product_by_gc_id( $_gc_id, true );
           $variation_post = new DRGC_Product( $variation_post_id, 'dr_product_variation' );
           $variation_post->set_data( $variation_product );
           $variation_post->set_parent( $parent_post->id );
@@ -138,8 +143,8 @@ class DRGC_Product_Importer {
       }
 
       // Clear cache after the last product is imported
-      if ( $idx === count( $this->dr_products ) - 1 ) {
-        delete_transient( 'dr_products' );
+      if ( $idx === count( $dr_product_ids ) - 1 ) {
+        delete_transient( 'dr_product_ids' );
       }
 
       if ( $this->type === 'AJAX' ) {
@@ -148,19 +153,6 @@ class DRGC_Product_Importer {
         return true;
       }
     }
-  }
-
-  private function basic_product_query_params() {
-    $required_attrs = [
-      'product.id',
-      'product.sku',
-      'product.baseProduct',
-      'product.displayableProduct',
-      'product.variations',
-      'product.variationAttributes',
-      'product.variations.product.customAttributes'
-    ];
-    return array( 'expand' => implode( ',', $required_attrs ) );
   }
 
   private function filter_out_share_product() {
